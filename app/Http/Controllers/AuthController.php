194ce01bd313a1a5f1a2116cd1cf4f9d7260cebd
Sreerajnr2212 <snrckd@gmail.com>
2024-07-19 19:20:55 +0530

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
     public function auth_login_admin(Request $request){
         $remember = !empty($request->remember)?true:false;
         if(Auth::attempt(['email'=>$request->email,'password'=>$request->password,'is_admin'=>1],$remember)){
-            return redirect('admin/form/add');
+            return redirect('admin/form/list');
         }else{
             return redirect()->back()->with('error','Please Enter Valid Email and Password');
         }
